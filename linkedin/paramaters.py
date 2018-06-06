79c893071bd3906c76e9436252cb4301978a23b2
IvanKwokKC <39858630+IvanKwokKC@users.noreply.github.com>
2018-06-06 22:54:54 +0800

    Add files via upload

---
+++ linkedin/paramaters.py
@@ -0,0 +1,5 @@
+search_query = 'site:linkedin.com/in/ AND "python developer" AND "London"'
+file_name = 'results_file.csv'
+
+linkedin_username = 'seamonster2001c@yahoo.com.hk'
+linkedin_password = '314601'
